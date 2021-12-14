e0ffa0921f2178201b44ba229dd22f45dda96c44
Mark Liu <manlge168@gmail.com>
2021-12-14 12:03:44 +0800

    Put the relevant code in a block

--- examples/session_example.rs
+++ examples/session_example.rs
@@ -63,105 +63,117 @@ fn run() -> Result<()> {
     //     .unwrap_or_default();
 
     //create_timeseries
-    session.create_timeseries(
-        "root.sg_rs.dev2.status",
-        TSDataType::Float,
-        TSEncoding::Plain,
-        TSCompressionType::SNAPPY,
-        None,
-        None,
-        None,
-        None,
-    )?;
-    session.delete_timeseries(vec!["root.sg_rs.dev2.status"])?;
+    {
+        session.create_timeseries(
+            "root.sg_rs.dev2.status",
+            TSDataType::Float,
+            TSEncoding::Plain,
+            TSCompressionType::SNAPPY,
+            None,
+            None,
+            None,
+            None,
+        )?;
+        session.delete_timeseries(vec!["root.sg_rs.dev2.status"])?;
+    }
 
     //create_multi_timeseries
-    session.create_multi_timeseries(
-        vec!["root.sg3.dev1.temperature", "root.sg3.dev1.desc"],
-        vec![TSDataType::Float, TSDataType::Text],
-        vec![TSEncoding::Plain, TSEncoding::Plain],
-        vec![TSCompressionType::SNAPPY, TSCompressionType::SNAPPY],
-        None,
-        None,
-        None,
-        None,
-    )?;
-    session.delete_timeseries(vec!["root.sg3.dev1.temperature", "root.sg3.dev1.desc"])?;
+    {
+        session.create_multi_timeseries(
+            vec!["root.sg3.dev1.temperature", "root.sg3.dev1.desc"],
+            vec![TSDataType::Float, TSDataType::Text],
+            vec![TSEncoding::Plain, TSEncoding::Plain],
+            vec![TSCompressionType::SNAPPY, TSCompressionType::SNAPPY],
+            None,
+            None,
+            None,
+            None,
+        )?;
+        session.delete_timeseries(vec!["root.sg3.dev1.temperature", "root.sg3.dev1.desc"])?;
+    }
 
     //insert_record
-    session.insert_record(
-        "root.sg_rs.dev5",
-        vec!["online", "desc"],
-        vec![Value::Bool(false), Value::Text("F4145".to_string())],
-        Local::now().timestamp_millis(),
-        false,
-    )?;
-    session.delete_timeseries(vec!["root.sg_rs.dev5.online", "root.sg_rs.dev5.desc"])?;
+    {
+        session.insert_record(
+            "root.sg_rs.dev5",
+            vec!["online", "desc"],
+            vec![Value::Bool(false), Value::Text("F4145".to_string())],
+            Local::now().timestamp_millis(),
+            false,
+        )?;
+        session.delete_timeseries(vec!["root.sg_rs.dev5.online", "root.sg_rs.dev5.desc"])?;
+    }
 
     //insert_string_record
-    session.insert_string_record(
-        "root.sg_rs.wf02.wt02",
-        vec!["id", "location"],
-        vec!["SN:001", "BeiJing"],
-        Local::now().timestamp_millis(),
-        false,
-    )?;
-    session.delete_timeseries(vec![
-        "root.sg_rs.wf02.wt02.id",
-        "root.sg_rs.wf02.wt02.location",
-    ])?;
+    {
+        session.insert_string_record(
+            "root.sg_rs.wf02.wt02",
+            vec!["id", "location"],
+            vec!["SN:001", "BeiJing"],
+            Local::now().timestamp_millis(),
+            false,
+        )?;
+        session.delete_timeseries(vec![
+            "root.sg_rs.wf02.wt02.id",
+            "root.sg_rs.wf02.wt02.location",
+        ])?;
+    }
 
     //insert_records
-    session.insert_records(
-        vec!["root.sg_rs.dev1"],
-        vec![vec![
-            "restart_count",
-            "tick_count",
-            "price",
-            "temperature",
-            "description",
-            "status",
-        ]],
-        vec![vec![
-            Value::Int32(1),
-            Value::Int64(2018),
-            Value::Double(1988.1),
-            Value::Float(12.1),
-            Value::Text("Test Device 1".to_string()),
-            Value::Bool(false),
-        ]],
-        vec![Local::now().timestamp_millis()],
-    )?;
-    session.delete_timeseries(vec![
-        "root.sg_rs.dev1.restart_count",
-        "root.sg_rs.dev1.tick_count",
-        "root.sg_rs.dev1.price",
-        "root.sg_rs.dev1.temperature",
-        "root.sg_rs.dev1.description",
-        "root.sg_rs.dev1.status",
-    ])?;
+    {
+        session.insert_records(
+            vec!["root.sg_rs.dev1"],
+            vec![vec![
+                "restart_count",
+                "tick_count",
+                "price",
+                "temperature",
+                "description",
+                "status",
+            ]],
+            vec![vec![
+                Value::Int32(1),
+                Value::Int64(2018),
+                Value::Double(1988.1),
+                Value::Float(12.1),
+                Value::Text("Test Device 1".to_string()),
+                Value::Bool(false),
+            ]],
+            vec![Local::now().timestamp_millis()],
+        )?;
+        session.delete_timeseries(vec![
+            "root.sg_rs.dev1.restart_count",
+            "root.sg_rs.dev1.tick_count",
+            "root.sg_rs.dev1.price",
+            "root.sg_rs.dev1.temperature",
+            "root.sg_rs.dev1.description",
+            "root.sg_rs.dev1.status",
+        ])?;
+    }
 
     //insert_records_of_one_device
-    session.insert_records_of_one_device(
-        "root.sg_rs.dev0",
-        vec![
-            Local::now().timestamp_millis(),
-            Local::now().timestamp_millis() - 1,
-        ],
-        vec![
-            vec!["restart_count", "tick_count", "price"],
-            vec!["temperature", "description", "status"],
-        ],
-        vec![
-            vec![Value::Int32(1), Value::Int64(2018), Value::Double(1988.1)],
+    {
+        session.insert_records_of_one_device(
+            "root.sg_rs.dev0",
             vec![
-                Value::Float(36.8),
-                Value::Text("thermograph".to_string()),
-                Value::Bool(false),
+                Local::now().timestamp_millis(),
+                Local::now().timestamp_millis() - 1,
             ],
-        ],
-        false,
-    )?;
+            vec![
+                vec!["restart_count", "tick_count", "price"],
+                vec!["temperature", "description", "status"],
+            ],
+            vec![
+                vec![Value::Int32(1), Value::Int64(2018), Value::Double(1988.1)],
+                vec![
+                    Value::Float(36.8),
+                    Value::Text("thermograph".to_string()),
+                    Value::Bool(false),
+                ],
+            ],
+            false,
+        )?;
+    }
 
     //table
     let mut ts = Local::now().timestamp_millis();
@@ -175,112 +187,125 @@ fn run() -> Result<()> {
 
     //insert_tablet
     session.insert_tablet(&tablet1)?;
-    tablet2.sort();
-    tablet3.sort();
 
     //insert_tablets
-    session.insert_tablets(vec![&tablet2, &tablet3])?;
-    session.insert_records_of_one_device(
-        "root.sg_rs.dev1",
-        vec![1, 16],
-        vec![vec!["status"], vec!["status"]],
-        vec![vec![Value::Bool(true)], vec![Value::Bool(true)]],
-        true,
-    )?;
+    {
+        tablet2.sort();
+        tablet3.sort();
+        session.insert_tablets(vec![&tablet2, &tablet3])?;
+        session.insert_records_of_one_device(
+            "root.sg_rs.dev1",
+            vec![1, 16],
+            vec![vec!["status"], vec!["status"]],
+            vec![vec![Value::Bool(true)], vec![Value::Bool(true)]],
+            true,
+        )?;
+    }
 
     //delete_data
     session.delete_data(vec!["root.sg_rs.dev1.status"], 1, 16)?;
 
     //execute_query_statement
-    let dataset = session.execute_query_statement("select * from root.sg_rs.device2", None)?;
-    // Get columns, column types and values from the dataset
-    // For example:
-    let width = 18;
-    let column_count = dataset.get_column_names().len();
-    let print_line_sep = || println!("{:=<width$}", '=', width = (width + 1) * column_count + 1);
+    {
+        let dataset = session.execute_query_statement("select * from root.sg_rs.device2", None)?;
+        // Get columns, column types and values from the dataset
+        // For example:
+        let width = 18;
+        let column_count = dataset.get_column_names().len();
+        let print_line_sep =
+            || println!("{:=<width$}", '=', width = (width + 1) * column_count + 1);
 
-    print_line_sep();
-    dataset
-        .get_column_names()
-        .iter()
-        .for_each(|c| print!("|{:>width$}", c.split('.').last().unwrap(), width = width));
-    print!("|\n");
-    print_line_sep();
-    dataset.get_data_types().iter().for_each(|t| {
-        let type_name = format!("{:?}", t);
-        print!("|{:>width$}", type_name, width = width)
-    });
-    print!("|\n");
-    print_line_sep();
-    dataset.for_each(|r| {
-        r.values.iter().for_each(|v| match v {
-            Value::Bool(v) => print!("|{:>width$}", v, width = width),
-            Value::Int32(v) => print!("|{:>width$}", v, width = width),
-            Value::Int64(v) => print!("|{:>width$}", v, width = width),
-            Value::Float(v) => print!("|{:>width$}", v, width = width),
-            Value::Double(v) => print!("|{:>width$}", v, width = width),
-            Value::Text(v) => print!("|{:>width$}", v, width = width),
-            Value::Null => print!("|{:>width$}", "null", width = width),
+        print_line_sep();
+        dataset
+            .get_column_names()
+            .iter()
+            .for_each(|c| print!("|{:>width$}", c.split('.').last().unwrap(), width = width));
+        print!("|\n");
+        print_line_sep();
+        dataset.get_data_types().iter().for_each(|t| {
+            let type_name = format!("{:?}", t);
+            print!("|{:>width$}", type_name, width = width)
         });
         print!("|\n");
-    });
-    print_line_sep();
+        print_line_sep();
+        dataset.for_each(|r| {
+            r.values.iter().for_each(|v| match v {
+                Value::Bool(v) => print!("|{:>width$}", v, width = width),
+                Value::Int32(v) => print!("|{:>width$}", v, width = width),
+                Value::Int64(v) => print!("|{:>width$}", v, width = width),
+                Value::Float(v) => print!("|{:>width$}", v, width = width),
+                Value::Double(v) => print!("|{:>width$}", v, width = width),
+                Value::Text(v) => print!("|{:>width$}", v, width = width),
+                Value::Null => print!("|{:>width$}", "null", width = width),
+            });
+            print!("|\n");
+        });
+        print_line_sep();
+    }
 
     //execute_statement
-    let dataset = session.execute_statement("show timeseries", None)?;
-    let mut table = Table::new();
-    table.set_titles(Row::new(
-        dataset
-            .get_column_names()
-            .iter()
-            .map(|c| cell!(c))
-            .collect(),
-    ));
-    dataset.for_each(|r: RowRecord| {
-        table.add_row(Row::new(
-            r.values.iter().map(|v: &Value| cell!(v)).collect(),
+    {
+        let dataset = session.execute_statement("show timeseries", None)?;
+        let mut table = Table::new();
+        table.set_titles(Row::new(
+            dataset
+                .get_column_names()
+                .iter()
+                .map(|c| cell!(c))
+                .collect(),
         ));
-    });
-    table.printstd();
+        dataset.for_each(|r: RowRecord| {
+            table.add_row(Row::new(
+                r.values.iter().map(|v: &Value| cell!(v)).collect(),
+            ));
+        });
+        table.printstd();
+    }
 
     //execute_batch_statement
-    session.execute_batch_statement(vec![
-        "insert into root.sg_rs.dev6(time,s5) values(1,true)",
-        "insert into root.sg_rs.dev6(time,s5) values(2,true)",
-        "insert into root.sg_rs.dev6(time,s5) values(3,true)",
-    ])?;
-
+    {
+        session.execute_batch_statement(vec![
+            "insert into root.sg_rs.dev6(time,s5) values(1,true)",
+            "insert into root.sg_rs.dev6(time,s5) values(2,true)",
+            "insert into root.sg_rs.dev6(time,s5) values(3,true)",
+        ])?;
+    }
     //execute_raw_data_query
-    let dataset = session.execute_raw_data_query(
-        vec![
-            "root.sg_rs.device2.restart_count",
-            "root.sg_rs.device2.tick_count",
-            "root.sg_rs.device2.description",
-        ],
-        0,
-        i64::MAX,
-    )?;
-    let mut table = Table::new();
-    table.set_titles(Row::new(
-        dataset
-            .get_column_names()
-            .iter()
-            .map(|c| cell!(c))
-            .collect(),
-    ));
-    dataset.for_each(|r: RowRecord| {
-        table.add_row(Row::new(
-            r.values.iter().map(|v: &Value| cell!(v)).collect(),
+    {
+        let dataset = session.execute_raw_data_query(
+            vec![
+                "root.sg_rs.device2.restart_count",
+                "root.sg_rs.device2.tick_count",
+                "root.sg_rs.device2.description",
+            ],
+            0,
+            i64::MAX,
+        )?;
+        let mut table = Table::new();
+        table.set_titles(Row::new(
+            dataset
+                .get_column_names()
+                .iter()
+                .map(|c| cell!(c))
+                .collect(),
         ));
-    });
-    table.printstd();
+        dataset.for_each(|r: RowRecord| {
+            table.add_row(Row::new(
+                r.values.iter().map(|v: &Value| cell!(v)).collect(),
+            ));
+        });
+        table.printstd();
+    }
 
     //execute_update_statement
-    if let Some(dataset) =
-        session.execute_update_statement("delete timeseries root.sg_rs.dev1.*")?
     {
-        dataset.for_each(|r| println!("timestamp: {} {:?}", r.timestamp, r.values));
+        if let Some(dataset) =
+            session.execute_update_statement("delete timeseries root.sg_rs.dev1.*")?
+        {
+            dataset.for_each(|r| println!("timestamp: {} {:?}", r.timestamp, r.values));
+        }
     }
+
     session.close()?;
     Ok(())
 }
